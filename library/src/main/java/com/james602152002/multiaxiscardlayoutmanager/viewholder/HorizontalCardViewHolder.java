9d45d08b00cbe2f1e371a98cb5d49fce52da98be
= <=>
2018-02-06 13:48:07 +0800

    develop

--- library/src/main/java/com/james602152002/multiaxiscardlayoutmanager/viewholder/HorizontalCardViewHolder.java
+++ library/src/main/java/com/james602152002/multiaxiscardlayoutmanager/viewholder/HorizontalCardViewHolder.java
@@ -1,5 +1,7 @@
 package com.james602152002.multiaxiscardlayoutmanager.viewholder;
 
+import android.content.res.Resources;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import com.james602152002.multiaxiscardlayoutmanager.interfaces.ViewHolderCallBack;
@@ -8,13 +10,19 @@ import com.james602152002.multiaxiscardlayoutmanager.interfaces.ViewHolderCallBa
  * Created by shiki60215 on 18-2-5.
  */
 
-public class HorizontalCardViewHolder extends BaseCardViewHolder{
+public class HorizontalCardViewHolder extends BaseCardViewHolder {
 
     private final ViewHolderCallBack callBack;
 
     public HorizontalCardViewHolder(View itemView, ViewHolderCallBack callBack) {
         super(itemView);
+        Resources resources = itemView.getResources();
+        itemView.setLayoutParams(new RecyclerView.LayoutParams((int) (resources.getDisplayMetrics().widthPixels * .5f), dp2px(resources, 100)));
         this.callBack = callBack;
+    }
+
+    private int dp2px(Resources resources, float dpValue) {
+        return (int) (0.5f + dpValue * resources.getDisplayMetrics().density);
     }
 
     @Override
