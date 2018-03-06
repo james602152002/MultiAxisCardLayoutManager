fdc77af1a566098e5a2985f7df30840af27e577a
= <=>
2018-03-06 10:48:31 +0800

    develop

---
+++ library/src/main/java/com/james602152002/multiaxiscardlayoutmanager/ui/CardRecyclerView.java
@@ -0,0 +1,25 @@
+package com.james602152002.multiaxiscardlayoutmanager.ui;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+
+/**
+ * Created by shiki60215 on 18-3-6.
+ */
+
+public class CardRecyclerView extends RecyclerView {
+
+    public CardRecyclerView(Context context) {
+        super(context);
+    }
+
+    public CardRecyclerView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CardRecyclerView(Context context, @Nullable AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
